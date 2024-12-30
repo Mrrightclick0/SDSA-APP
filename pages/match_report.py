3f24809eac77f7f1455ff7a819ac0f7ff5ad6126
Ahmed Ashour Hayyan <113825427+ashayyan@users.noreply.github.com>
2024-12-30 13:59:54 +0300

    Update match_report.py

--- pages/match_report.py
+++ pages/match_report.py
@@ -1687,7 +1687,7 @@ def app():
                     ax.text(93, -3, f'Entry by Carry: {(adfcarry)}', fontsize=12, color=acol, ha='center', va='center')
                     ax.text(10, -3, f'Entry by Pass: {(hdfpass)}', fontsize=12, color=hcol, ha='center', va='center')
                     ax.text(40, -3, f'Entry by Carry: {(hdfcarry)}', fontsize=12, color=hcol, ha='center', va='center')
-                     home_data = {
+                    home_data = {
                             'Team_Name': hteamName,
                             'Total_Box_Entries': len(hbentry),
                             'Box_Entry_From_Left': len(hleft),
